0ae9ff7e8ea0f1ce0593ae319c085be0c8130040
Evgen <litvinskijzena@gmail.com>
2025-10-09 00:17:47 +0300

    update

--- HelloWorld/src/Factorial.java
+++ HelloWorld/src/Factorial.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Factorial {
+
+    public static long factorialRecursive(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Number must be non-negative");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorialRecursive(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial Calculator - Recursive");
+
+        int number = 5;
+        long result = factorialRecursive(number);
+        System.out.println(number + "! = " + result);
+    }
 }
